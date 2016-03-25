8203b8630f6ff3159057b92320ab29c816dacfc3
comstock <comstock@users.noreply.github.com>
2016-03-24 20:28:51 -0400

    Update VIAportfolioCaptions.py

--- VIAportfolioCaptions.py
+++ VIAportfolioCaptions.py
@@ -14,9 +14,8 @@ record_stem = "http://id.lib.harvard.edu/via/"
 record_tail = "/catalog"
 print "IMAGELINK COUNT = " + str(count)
 
-
-title = doc.getElementsByTagName('work.title')
-record = doc.getElementsByTagName('via_id')
+#title = doc.getElementsByTagName('work.title')
+#record = doc.getElementsByTagName('via_id')
 
 line1 = "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n\
 <head>\n\
@@ -70,6 +69,9 @@ for line in portfolio:
         line = re.sub("<group.subwork.*","",line)
         line = re.sub("<text>","",line)
         line = re.sub("</text>","",line)
+        if re.search(".*<type>.*",line):
+            print line
+            done
         #line = re.sub("<work.title>","",line)
         #line = re.sub("</work.title>","",line)
         line2c = "<figcaption>" + line + "</figcaption>\n" + line3 + "</figure>"+ "\n\n"
@@ -84,8 +86,9 @@ for line in portfolio:
         line = re.sub("<group.subwork.*","",line)
         line = re.sub("<text>","",line)
         line = re.sub("</text>","",line)
-        #line = re.sub("<work.title>","",line)
-        #line = re.sub("</work.title>","",line)
+        if re.search(".*<type>.*",line):
+            print line
+            break
         line2c = "<figcaption>" + line + "</figcaption>\n" + line3 + "</figure>"+ "\n\n"
         #print line2c
         #print line3
